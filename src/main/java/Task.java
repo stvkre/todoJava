f1d1a0893187e4f3251d2dbdf94e9b2a03f02433
stvkre <kiariemburuh@gmail.com>
2017-05-01 15:42:05 +0300

    task.java,DB.java
    Connecting the app to an SQL database

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
+  private String description;
+  private boolean completed;
+  private LocalDateTime createdAt;
+  private int id;
 
   public Task(String description) {
-
+    this.description = description;
+    completed = false;
+    createdAt = LocalDateTime.now();
   }
-
-}
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,11 +1,18 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TaskTest {
-  @Test
-  public void Task_instantiatesCorrectly_true() {
-    Task myTask = new Task("Mow the lawn");
-    assertEquals(true, myTask instanceof Task);
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
   }
 
-}
+  @After
+ public void tearDown() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "DELETE FROM tasks *;";
+     con.createQuery(sql).executeUpdate();
+   }
+ }
