faebeb431dce05bd4145974bf98d2fef8a9d6fd0
stvkre <kiariemburuh@gmail.com>
2017-05-03 16:09:10 +0300

    DatabaseRule.java
    overriding methods to create a new database connection

--- src/main/java/DatabaseRule.java
+++ src/main/java/DatabaseRule.java
@@ -3,4 +3,19 @@ import org.sql2o.*;
 
 public class DatabaseRule extends ExternalResource {
 
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+
 }
